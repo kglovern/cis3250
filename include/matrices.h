aeb6efa0465165ceac3871aa65d918b2dd1f8fbb
Nicholas Skoretz <nskoretz@uoguelph.ca>
2017-10-17 11:25:40 -0400

    Committing my refactored code

--- include/matrices.h
+++ include/matrices.h
@@ -1,120 +1,51 @@
-#include <stdio.h>
-
-void matrix_sum(){
-
-int i,j,n1,m1,n2,m2;
-
-printf("Enter the number of rows of matrix 1:");
-scanf("%d",&n1);
-printf("Enter the number of columns of matrix :");
-scanf("%d",&m1);
-printf("Enter the number of rows of matrix 2:");
-scanf("%d",&n2);
-printf("Enter the number of columns of matrix 2:");
-scanf("%d",&m2);
-if(n1==n2&&m1==m2){
-
-float a[n1][m1];
-float b[n2][m2];
-float c[n1][m1];
+/**
+* matrices.h
+*
+* This code has been refactored by Nicholas Skoretz to conform to
+* Team 1 Coding Conventions Document.
+*
+* Last Modified: 2017-10-15
+* Modified by: Nicholas Skoretz
+*
+* This file declares all functions related to the matrix operations of the
+* calculator.
+*/
 
 
-for(i=0;i<n1;i++){
-        printf("Enter the members of matrix 1 row %d :",i+1);
-    for(j=0;j<m1;j++){
-        scanf("%f",&a[i][j]);
-    }
-}
-for(i=0;i<n2;i++){
-        printf("Enter the members of matrix 2 row %d :",i+1);
-    for(j=0;j<m2;j++){
-        scanf("%f",&b[i][j]);
-    }
-}
-for(i=0;i<n1;i++){
-    for(j=0;j<m1;j++){
-        c[i][j]=a[i][j]+b[i][j];
-    }
-}
-  printf("The sum of both matrices is\n:");
-        for(j=0;j<m1;j++){
-            for(i=0;i<n1;i++){
-                printf("\t\t %.0f",c[i][j]);}
-                printf("\n");}}
-    else{
-        printf("\n\tThe matrices are incompatible");
-    }
-        }
+//Header Guards
+#ifndef MATRICES_H
+#define MATRICES_H
 
 
-void matrix_product(){
-
-int i,j,n1,m1,n2,m2,l,k;
-
-printf("Enter the number of rows of matrix 1:");
-scanf("%d",&n1);
-printf("Enter the number of columns of matrix :");
-scanf("%d",&m1);
-printf("Enter the number of rows of matrix 2:");
-scanf("%d",&n2);
-printf("Enter the number of columns of matrix 2:");
-scanf("%d",&m2);
-
-float a[n1][m1];
-float b[n2][m2];
-float c[n1][m1];
-for(i=0;i<n1;i++){
-        printf("Enter the members of matrix 1 row %d :",i+1);
-    for(j=0;j<m1;j++){
-        scanf("%f",&a[i][j]);
-    }
-}
-for(i=0;i<n2;i++){
-        printf("Enter the members of matrix 2 row %d :",i+1);
-    for(j=0;j<m2;j++){
-        scanf("%f",&b[i][j]);
-    }
-}
-c[i][j]=1;
- for(i=0;i<n1;i++){
-    for(j=0;j<m2;j++){
-                c[i][j]=0;
-                c[i][j]+=a[i][j]*b[i][j];
-        }
-    }
+//Functions
+/**
+* This function calculates and displays the sum of two matrices entered by the
+* user.
+*
+* @return void      No return, prints to stdout.
+*/
+void matrix_sum ();
 
 
-    printf("The  of the matrix is\n:");
-        for(j=0;j<m1;j++){
-            for(i=0;i<n2;i++){
-                printf("\t\t %.0f",a[i][j]);}
-                printf("\n");}
 
 
-}
+/**
+* This function calculates and displays the product of two matrices entered
+* by the user.
+*
+* @return void      Output to stdout.
+*/
+void matrix_product ();
 
-void matrix_transpose(){
 
-int i,j,n,m;
 
-printf("Enter the number of rows:");
-scanf("%d",&n);
-printf("Enter the number of columns:");
-scanf("%d",&m);
 
-float a[n][m];
+/**
+* This function transposes a matrix entered by the user. That is to say, it
+* switches their row and column indexes to 'flip' the matrix.
+*
+* @return void      Prints to stdout.
+*/
+void matrix_transpose ();
 
-for(i=0;i<n;i++){
-        printf("Enter the members of row %d :",i+1);
-    for(j=0;j<m;j++){
-        scanf("%f",&a[i][j]);
-    }
-}
-
-        printf("The transpose of the matrix is\n:");
-        for(j=0;j<m;j++){
-            for(i=0;i<n;i++){
-                printf("\t\t %.0f",a[i][j]);}
-                printf("\n");}
-        }
-
+#endif
